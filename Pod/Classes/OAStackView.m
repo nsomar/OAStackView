9f477461a1e3cec266cf9b26a9e4a45e11b9cbd8
Miguel Cabeça <miguel.cabeca@gmail.com>
2015-07-20 17:25:37 +0200

    Trigger arranged views layout when changing margin related properties.

--- Pod/Classes/OAStackView.m
+++ Pod/Classes/OAStackView.m
@@ -154,6 +154,16 @@
   self.distribution = distributionValue;
 }
 
+- (void)setLayoutMargins:(UIEdgeInsets)layoutMargins {
+    _layoutMargins = layoutMargins;
+    [self layoutArrangedViews];
+}
+
+- (void)setLayoutMarginsRelativeArrangement:(BOOL)layoutMarginsRelativeArrangement {
+    _layoutMarginsRelativeArrangement = layoutMarginsRelativeArrangement;
+    [self layoutArrangedViews];
+}
+
 #pragma mark Layouting
 
 - (void)layoutSubviews {
